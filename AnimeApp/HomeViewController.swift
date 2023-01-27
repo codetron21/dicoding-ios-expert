4635b1d875ca18e5a67e734ce9454834a3d2980a
codetron21 <codetronclan@gmail.com>
2023-01-27 16:36:30 +0700

    init project

---
+++ AnimeApp/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeApp
+//
+//  Created by Irsyad Purbha on 27/01/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+
+
+}
+
---
+++ AnimeApp/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  AnimeApp
+//
+//  Created by Irsyad Purbha on 27/01/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let navigation = UINavigationController(rootViewController: HomeViewController())
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = navigation
+        window?.makeKeyAndVisible()
+    }
+    
+}
+
