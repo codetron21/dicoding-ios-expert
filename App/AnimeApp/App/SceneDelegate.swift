7664786faf4b4ce37b1fb7dd8d8768a67beab31c
codetron21 <codetronclan@gmail.com>
2023-02-03 14:34:09 +0700

    update

--- App/AnimeApp/App/SceneDelegate.swift
+++ App/AnimeApp/App/SceneDelegate.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import Core
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        print(hello())
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
---
+++ Core/Core/Hello.swift
@@ -0,0 +1,12 @@
+//
+//  Hello.swift
+//  Core
+//
+//  Created by Irsyad Purbha on 03/02/23.
+//
+
+import Foundation
+
+public func hello() -> String {
+    return "Hello world"
+}
